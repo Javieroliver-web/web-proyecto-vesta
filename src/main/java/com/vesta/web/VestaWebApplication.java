7db14d1ecff7800f72a9f66d04a811e4da78f7ac
Javieroliver-web <javip200555@gmail.com>
2025-12-12 14:39:44 +0100

    Correccion de arranque

--- src/main/java/com/vesta/web/VestaWebApplication.java
+++ src/main/java/com/vesta/web/VestaWebApplication.java
@@ -2,8 +2,6 @@ package com.vesta.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class VestaWebApplication {
@@ -11,11 +9,5 @@ public class VestaWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(VestaWebApplication.class, args);
         System.out.println("🌐 WEB Vesta iniciada en el puerto 80");
-    }
-
-    // Registramos RestTemplate para poder inyectarlo en los servicios
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
